e0314dc23a7a1d0c21af49e8bca30d8263af3430
RRdD <ngabo.mugisha.bro@gmail.com>
2021-06-13 15:40:12 +0200

    changed webpack config

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-  const path = require("path");
-
-module.exports = {
-  devServer: {
-    contentBase: path.resolve(__dirname, "./src"),
-    historyApiFallback: true
-  },
-  entry: path.resolve(__dirname, "./src/index.js")
-};
